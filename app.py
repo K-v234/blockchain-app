a8c0c9d8a91cf08e992985c77fd241466102107c
Keerthivahanan <keerthivahananbkv@gmail.com>
2025-06-11 10:06:03 +0530

    fix: bind to 0.0.0.0 and dynamic port for Render

--- app.py
+++ app.py
@@ -439,4 +439,6 @@ def get_transactions_for_address(address):
 # -----------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
