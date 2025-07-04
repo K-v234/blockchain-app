f4bd615432f5becc6c7e7cd4c70910732869fd5f
Keerthivahanan <keerthivahananbkv@gmail.com>
2025-07-04 20:29:47 +0530

    ⚙️ Fix: Allow unsafe Werkzeug for Render deploy

--- app.py
+++ app.py
@@ -265,4 +265,5 @@ def tokenomics():
     })
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=5001, debug=False, use_reloader=False)
+    socketio.run(app, host='0.0.0.0', port=10000, allow_unsafe_werkzeug=True)
+
